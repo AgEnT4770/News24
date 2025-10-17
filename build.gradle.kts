f82992469e3204e97a6db0e3f4aa14c3f62ed4fc
Mohamed Ibrahim <mohamed.ibrahimturkey@gmail.com>
2025-10-17 17:15:32 +0300

    editing the design

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 plugins {
     // Register the plugins here that you want to use in your sub-projects
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
